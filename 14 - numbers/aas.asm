cd46ed7cb23ad9ab9a9ddf9775500374b56d85cc
Tiago Ceccon <ceccon.t@hotmail.com>
2022-04-25 23:19:46 -0300

    Code along numbers example

---
+++ 14 - numbers/aas.asm
@@ -0,0 +1,34 @@
+; The following example uses the AAS instruction (ASCII Adjust After Subtraction)
+
+section .text 
+    global _start               ; must be declared for using gcc 
+
+_start:                         ; tell linker entry point
+    sub ah, ah 
+    mov al, '9'
+    sub al, '3'
+    aas 
+    or al, 30h
+    mov [res], ax 
+
+    mov edx, len                ; message length
+    mov ecx, msg                ; message to write 
+    mov ebx, 1                  ; file descriptor
+    mov eax, 4                  ; system call number (sys_write)
+    int 0x80                    ; call kernel
+
+    mov edx, 1                  ; message length 
+    mov ecx, res                ; message to write 
+    mov ebx, 1                  ; file descriptor (stdout)
+    mov eax, 4                  ; system call number (sys_write)
+    int 0x80                    ; call kernel 
+
+    mov eax, 1                  ; system call number (sys_exit)
+    int 0x80                    ; call kernel 
+
+section .data 
+msg db 'The result is:', 0xA, 0xD
+len equ $ - msg 
+
+section .bss 
+res resb 1
